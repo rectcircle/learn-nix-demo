d67a50cc18c30f2c9c7fbd6e07a65984d8403cdb
rectcircle <rectcircle96@gmail.com>
2023-03-19 19:45:54 +0800

    简简单实现一个 nix 二进制缓存聚合器

---
+++ nix-binary-cache-aggregator/go.mod
@@ -0,0 +1,3 @@
+module github.com/rectcircle/learn-nix-demo/nix-binary-cache-aggregator
+
+go 1.19
---
+++ nix-binary-cache-aggregator/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+)
+
+type upstream struct {
+	URL      string
+	UseProxy bool
+}
+
+func newHandle(httpProxy string, upstreams []upstream) (func(w http.ResponseWriter, r *http.Request), error) {
+	client := http.DefaultClient
+	proxyClient := http.DefaultClient
+	if httpProxy != "" {
+		httpProxyURL, err := url.Parse(httpProxy)
+		if err != nil {
+			return nil, err
+		}
+		proxyClient = &http.Client{Transport: &http.Transport{Proxy: http.ProxyURL(httpProxyURL)}}
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// 打印访问日志
+		log.Printf("path: %s", r.URL.Path)
+		// TODO 优先从缓存中读取。
+		// 尝试从 upstream 中获取
+		var resp *http.Response
+		for i, u := range upstreams {
+			targetPath := path.Join(u.URL, r.URL.Path)
+			c := client
+			if u.UseProxy {
+				c = proxyClient
+			}
+			_resp, err := c.Get(targetPath)
+			if err != nil {
+				fmt.Printf("  try upstream[%d]: %s, error: %s", i, targetPath, err)
+				continue
+			}
+			if _resp.StatusCode != 200 {
+				fmt.Printf("  try upstream[%d]: %s, status code is: %d", i, targetPath, _resp.StatusCode)
+				_resp.Body.Close()
+				continue
+			}
+			resp = _resp
+		}
+		if resp == nil {
+			fmt.Printf("  all upstream not found")
+			w.WriteHeader(404)
+			return
+		}
+		defer resp.Body.Close()
+		w.WriteHeader(resp.StatusCode)
+		_, _ = io.Copy(w, resp.Body)
+		// TODO 起一个协程写入缓存中。
+	}, nil
+}
+
+func main() {
+	upstreams := []upstream{
+		{
+			URL:      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store",
+			UseProxy: false,
+		},
+		{
+			URL:      "https://cache.nixos.org",
+			UseProxy: true,
+		},
+	}
+	httpProxy := os.Getenv("HTTP_PROXY")
+	os.Unsetenv("HTTP_PROXY")
+	handle, err := newHandle(httpProxy, upstreams)
+	if err != nil {
+		panic(err)
+	}
+	http.HandleFunc("/", handle)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		panic(err)
+	}
+}
